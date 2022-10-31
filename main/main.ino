b90ebfdad288fd87a146b3057daf2e8670e6006c
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2022-10-31 10:09:03 +0100

    SD card start cyle

--- main/main.ino
+++ main/main.ino
@@ -14,6 +14,15 @@ LYCÉE MOLIÈRE ZARAGOZA
 
 void setup() {
   Serial.begin(9600);
+
+  // SD card initialization
+  Serial.print(F("Starting SD card"));
+  if (!SD.begin(10))
+  {
+    Serial.println(F("Error starting SD card"));
+    return;
+  }
+  Serial.println(F("SD card started successfully"));
 }
 
 void loop() {}

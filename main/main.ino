3bbfa88e835fa3e37fd6e364df01118028c89348
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2022-10-31 10:01:06 +0100

    Base main file

---
+++ main/main.ino
@@ -0,0 +1,19 @@
+/*
+
+ARDUINO SERVET VIII
+LYCÉE MOLIÈRE ZARAGOZA
+
+*/
+
+// Library importation
+
+// Used pins declaration
+
+// Global variables
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {}

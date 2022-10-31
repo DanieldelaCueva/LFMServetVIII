f87683c560d9265a7848b55886e7931b6c32d6ce
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2022-10-31 10:12:41 +0100

    SD write base cycle

--- main/main.ino
+++ main/main.ino
@@ -6,6 +6,7 @@ LYCÉE MOLIÈRE ZARAGOZA
 */
 
 // Library importation
+#include <SD.h>
 
 // Used pins declaration
 
@@ -25,4 +26,18 @@ void setup() {
   Serial.println(F("SD card started successfully"));
 }
 
-void loop() {}
+void loop() {
+
+    // opens or creates file datalog.txt to write it
+    logFile = SD.open("datalog.txt", FILE_WRITE);
+
+    // writes time from script's start in ms if the file is successfully opened, prints error otherwise
+    if (logFile) { 
+        logFile.print("Time (ms)=");
+        logFile.println(millis());
+        logFile.close();
+  
+  } else {
+    Serial.println("Error opening the file");
+  }
+}

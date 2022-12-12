3a5bf793b1b69d0109037864b96b996eadd7a152
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2022-12-12 21:01:55 +0100

    Added code for DHT22 temp+hum sensor

--- main/main.ino
+++ main/main.ino
@@ -6,12 +6,23 @@ LYCÉE MOLIÈRE ZARAGOZA
 */
 
 // Library importation
-#include <SD.h>
+
+  //SD library
+  #include <SD.h>
+  #include <Wire.h>
+
+  //TEMP+HUM library (DHT22)
+  #include <DHT.h>
+
+  #define DHTPIN 2
+  #define DHTTYPE DHT22
 
 // Used pins declaration
 
 // Global variables
+File logFile;
 
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
@@ -23,21 +34,34 @@ void setup() {
     Serial.println(F("Error starting SD card"));
     return;
   }
-  Serial.println(F("SD card started successfully"));
+  Serial.println(F("SD card started successfully")); 
 }
 
 void loop() {
+  delay(100);
 
-    // opens or creates file datalog.txt to write it
-    logFile = SD.open("datalog.txt", FILE_WRITE);
+  // temp + hum read
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
-    // writes time from script's start in ms if the file is successfully opened, prints error otherwise
-    if (logFile) { 
-        logFile.print("Time (ms)=");
-        logFile.println(millis());
-        logFile.close();
+  if (isnan(t) || isnan(h)){
+      Serial.print("Error reading DHT22");
+  }
+  
+   // opens or creates file datalog.txt to write it
+   logFile = SD.open("datalog.txt", FILE_WRITE);
+
+   // writes time from script's start in ms if the file is successfully opened, prints error otherwise
+   if (logFile) { 
+    logFile.print("Time (ms): ");
+    logFile.println(millis());
+    logFile.print("Temperature (ºC): ");
+    logFile.println(t);
+    logFile.print("Humidity (%): ");
+    logFile.println(h);
   
   } else {
     Serial.println("Error opening the file");
   }
+  delay(500);
 }
